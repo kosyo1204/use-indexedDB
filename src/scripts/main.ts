6b074266f7d99826034b72f93eb31fbef23c690a
kosyo1204 <kosyo1204@users.noreply.github.com>
2023-12-18 21:23:20 +0900

    indexedDB setup

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,16 +1,46 @@
-import { TodoModel } from "./todoModel";
-import { TodoView } from "./todoView";
-import { TodoController } from "./todoController";
+import Dexie from 'dexie';
 
-const model = new TodoModel();
-const view = new TodoView();
-const controller = new TodoController(model, view);
+const Dbconfig = {
+  name: 'MyDatabase',
+  table: 'books',
+}
 
-// Initial rendering
-controller.updateView();
+const db = new Dexie(Dbconfig.name);
+db.version(1).stores({
+  books: '++id, author'
+});
 
-// Simulate user actions
-controller.handleAddTodo("Buy groceries");
-controller.handleAddTodo("Finish coding");
-controller.handleAddTodo("Read a book");
-controller.handleRemoveTodo(1);
+document.getElementById('addButton')!.addEventListener('click', () => {
+
+});
+
+document.getElementById('loadButton')!.addEventListener('click', () => {
+
+});
+
+document.getElementById('clearButton')!.addEventListener('click', () => {
+
+});
+
+document.getElementById('resetDbButton')!.addEventListener('click', () => {
+
+});
+
+//初期ロードイベント
+document.addEventListener('DOMContentLoaded', () => {
+  // loadTable();
+});
+
+
+/** 
+ * DBからデータを取得
+ * 取得したデータを元にHTMLを作成
+ * 画面に表示
+* */
+
+/**
+ * データ追加時はDBへ保存した後にページを表示
+ * 表示する関数を用意すればOK
+ * 取得、追加、削除を終えた上で適用できそうなパターンを検討する
+ * 削除するところをリポジトリパターンに任せることができないか、など。
+ */
--- src/scripts/todoView.ts
+++
@@ -1,8 +0,0 @@
-export class TodoView {
-  render(todos: string[]) {
-    console.log("ToDo List:");
-    todos.forEach((todo, index) => {
-      console.log(`${index + 1}. ${todo}`);
-    });
-  }
-}
