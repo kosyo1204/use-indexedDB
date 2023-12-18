a96e726e5fec413a85b63bb4157870c9e0599713
kosyo1204 <kosyo1204@users.noreply.github.com>
2023-12-18 22:15:34 +0900

    set base

--- doc/dexiedjs.md
+++ doc/dexiedjs.md
@@ -15,7 +15,23 @@ APIを簡単に扱える。
 Promiseベースなので、非同期処理を扱える。
 
 # 目標
-todoリストを作る
+リポジトリパターンの適用
+
+# わかったこと
+DB.table.clear  ストアからすべてのオブジェクトを削除する。完了したら返されたPromiseを解決または拒否
+DB.delete       DBを削除。完了したらPromiseのthenメソッドを呼び出す。
+
+# error
+型  の式を使用して型 'Dexie' にインデックスを付けることはできないため、要素は暗黙的に any 型になります。
+  型 'string' のパラメーターを持つインデックス シグネチャが型 'Dexie' に見つかりませんでした。ts(7053)
+
+
+jsonのimport
+  https://zenn.dev/poyomitech/articles/d6b8ffb42e3cd4
+
+# リファクタポイント
+
+
 
 ## 参考記事
 https://github.com/dexie/Dexie.js
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,46 +1,98 @@
 import Dexie from 'dexie';
+import jsonData from './data.json'; //JSON（初期データ）をインポート
 
-const Dbconfig = {
-  name: 'MyDatabase',
-  table: 'books',
+interface Book {
+  id: number;
+  title: string;
+  author: string;
 }
 
-const db = new Dexie(Dbconfig.name);
-db.version(1).stores({
-  books: '++id, author'
+const DbConfig = {
+  name: 'testDb',
+  table:'books'
+};
+
+//
+// IndexedDB アクセスインスタンス（Dexie）生成
+//
+const idb = new Dexie(DbConfig.name);
+idb.version(1).stores({
+  books: '++id, author'  //テーブル定義(先頭がキー、それ以外はインデックスカラム。全フィールド定義は必要はありません。)
 });
 
-document.getElementById('addButton')!.addEventListener('click', () => {
+/**
+ * データからHTMLのtable要素を生成します。
+ * @param books
+ * @returns 
+ */
+function generateTable(books: Book[]): HTMLTableElement {
+  const table = document.createElement('table');
+  const thead = table.createTHead();
+  const tbody = table.createTBody();
 
+  // ヘッダー行を作成
+  const headerRow = thead.insertRow();
+  ['ID', '書名', '著者'].forEach(text => {
+      const th = document.createElement('th');
+      th.textContent = text;
+      headerRow.appendChild(th);
+  });
+
+  // データ行を作成
+  books.forEach(book => {
+      const row = tbody.insertRow();
+      row.insertCell().textContent = book.id.toString();;
+      row.insertCell().textContent = book.title.toString();
+      row.insertCell().textContent = book.author.toString();
+    });
+
+  return table;
+}
+
+/**
+ * DBからデータをロードして、HTML要素を構築します。
+ * @returns
+ */
+async function loadTable() {
+  const books = await idb[DbConfig.table].toArray()
+  const container: HTMLElement|null = document.getElementById('table-container');
+  if (container === null) return;
+  if (container.firstChild) container.removeChild(container.firstChild);
+  container.appendChild(generateTable(books));
+}
+
+//
+// イベント処理
+//
+//データを１件追加（値はランダム生成）
+document.getElementById('addButton')!.addEventListener('click', async () => {
+  // 1 から 99999 までのランダムな整数を生成
+  const randomNum = Math.floor(Math.random() * 99999) + 1;
+  // 整数を文字列に変換し、5桁になるようにゼロで埋める
+  const suffix = randomNum.toString().padStart(5, '0');
+  await idb[DbConfig.table].add({title: 'Title-' + suffix, author: 'Author-' + suffix});
+  loadTable();
 });
 
-document.getElementById('loadButton')!.addEventListener('click', () => {
-
+//全データ削除
+document.getElementById('clearButton')!.addEventListener('click', async () => {
+  await idb[DbConfig.table].clear();
+  loadTable();
 });
 
-document.getElementById('clearButton')!.addEventListener('click', () => {
-
+//DB再構築（DB削除）
+document.getElementById('resetDbButton')!.addEventListener('click', async () => {
+  await idb.delete();
+  window.location.reload();  // DBを消すだけなので、要リロード
 });
 
-document.getElementById('resetDbButton')!.addEventListener('click', () => {
-
+//JSONデータをDBにインポート
+document.getElementById('loadButton')!.addEventListener('click', async () => {
+  await idb[DbConfig.table].bulkAdd(jsonData);
+  loadTable();
 });
 
 //初期ロードイベント
-document.addEventListener('DOMContentLoaded', () => {
-  // loadTable();
+document.addEventListener('DOMContentLoaded', async () => {
+  loadTable();
 });
-
-
-/** 
- * DBからデータを取得
- * 取得したデータを元にHTMLを作成
- * 画面に表示
-* */
-
-/**
- * データ追加時はDBへ保存した後にページを表示
- * 表示する関数を用意すればOK
- * 取得、追加、削除を終えた上で適用できそうなパターンを検討する
- * 削除するところをリポジトリパターンに任せることができないか、など。
- */
